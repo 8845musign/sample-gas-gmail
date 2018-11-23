4fb4153755646bde5461b83b2b9eb14865eecf76
8845musign <8845musign@gmail.com>
2018-11-24 01:27:10 +0900

    feat: access to gmail

--- src/Hello.ts
+++ src/Hello.ts
@@ -7,3 +7,12 @@ const greet = (name: string) => {
 function test () {
   const sheet = new Sheet(`${configConst.FILE_PREFIX}-sheet`)
 }
+
+function searchMail () {
+  const threads = GmailApp.search('お問い合わせ', 0, 30)
+  const msgs = GmailApp.getMessagesForThreads(threads)
+
+  msgs.forEach(msg => {
+    Logger.log(msg[0].getSubject())
+  })
+}
